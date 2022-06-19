dd0bcd2fb3b831ee10572f6d391c1a45fc00c8d6
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2022-06-19 17:23:37 +0000

    modified settings.py

--- app/core/settings.py
+++ app/core/settings.py
@@ -3,15 +3,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PROJECT_NAME = os.getenv("PROJECT_NAME", "FastAPI Project")
-API_V1_PREFIX = "/api/v1"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenvb(b"SECRET_KEY")
 if not SECRET_KEY:
     SECRET_KEY = os.urandom(32)
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_NAME = os.getenv("PROJECT_NAME", "FastAPI Project")
+API_V1_PREFIX = "/api/v1"
 
+# Templates Directory
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
+# Static files like CSS & Media files
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
